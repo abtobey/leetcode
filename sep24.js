66cd38b5550d5d9fa880eafbcdc9910b94b0d088
Aaron Tobey <abtobey@gmail.com>
2020-09-24 15:59:12 -0500

    better run time, not amazing though

--- sep24.js
+++ sep24.js
@@ -1,14 +1,13 @@
 var findTheDifference = function(s, t) {
-    let s2=s.split("").sort();
-    let t2=t.split("").sort();
-    let added ="";
+    let s2=s.split("");
+    let t2=t.split("");
     for(let i=0; i<t2.length; i++){
-        if(s2[i] !== t2[i]){
-            added= t2[i];
-            i=t2.length;
+        if(s2.includes(t2[i])){
+            s2.splice(s2.indexOf(t2[i]),1)
+        }else{
+            return t2[i];
         }
     }
-    return added;
 };
 
 let s="jklert";
